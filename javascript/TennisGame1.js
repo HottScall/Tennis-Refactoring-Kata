4a81a75dfed5b56f220ab6f3be62d0f4ccd0014e
Scott <Scotthall1983@hotmail.com>
2020-07-07 08:30:52 +0100

    Added additional notes for things to look at throughout the application. Using seperation of concerns and and setting the score and tempScore to the constructor functions and calling them later in the applcation.

--- javascript/TennisGame1.js
+++ javascript/TennisGame1.js
@@ -16,6 +16,10 @@ class TennisGame {
     }
   }
 
+  // This is a long, very messy and confusing method.
+  // Seperate the concerns maybe one for draw, one for deuce, one for point won
+  // Set score and and tempScore the the constructor function and call them later in the application?
+
   getScore() {
     var score = "";
     var tempScore = 0;
